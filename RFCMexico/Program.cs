566d81562f65153d532230908776fd365bba2a00
Gustavo Gilberto Godinho <gustavo.godinho@ituran.com.br>
2020-02-14 09:00:10 -0300

    primeira versao

---
+++ RFCMexico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RFCMexico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
